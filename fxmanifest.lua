8e6732c0b53c94330a804e05b5c5b29193d4782b
aidenharvey21-beep <aiden.harvey21@gmail.com>
2026-07-14 15:11:28 -0400

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,55 +1,57 @@
+```lua
 fx_version 'cerulean'
-game 'gta5'
 
-author 'Aiden Harvey'
-description 'AirSupportPro - Advanced standalone helicopter aviation suite for FiveM'
-version '0.1.0'
+game 'gta5'
 
 lua54 'yes'
 
--- Resource metadata
-repository 'https://github.com/aidenharvey21-beep/ASP'
 
--- Shared files
+author 'Aiden Harvey'
+
+description 'AirSupportPro - Advanced Helicopter Aviation Suite'
+
+version '0.2.0'
+
+
 shared_scripts {
+
     'config.lua',
-    'shared/*.lua'
+
+    'shared/helpers.lua'
+
 }
 
--- Client files
+
 client_scripts {
-    'client/*.lua'
+
+    'client/utilities.lua',
+
+    'client/camera.lua',
+
+    'client/main.lua',
+
+    'client/controls.lua'
+
 }
 
--- Server files
+
 server_scripts {
-    'server/*.lua'
+
+    'server/server.lua'
+
 }
 
--- NUI Interface
-ui_page 'html/index.html'
 
 files {
+
     'html/index.html',
+
     'html/style.css',
+
     'html/app.js'
+
 }
 
--- Optional dependencies
-dependencies {
-    '/onesync'
-}
 
--- Export definitions
-exports {
-    'IsAirSupportActive',
-    'GetCurrentTarget',
-    'GetCameraState',
-    'StartOrbit',
-    'StopOrbit'
-}
-
--- Resource settings
-escrow_ignore {
-    'config.lua'
-}
+ui_page 'html/index.html'
+```
