82e936f167f01d18e3b2ef77a3ad34574df7ac5f
aidenharvey21-beep <aiden.harvey21@gmail.com>
2026-07-14 16:46:20 -0400

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,8 @@
-```lua
 fx_version 'cerulean'
 
 game 'gta5'
 
 lua54 'yes'
-
 
 author 'Aiden Harvey'
 
@@ -14,44 +12,29 @@ version '0.2.0'
 
 
 shared_scripts {
-
     'config.lua',
-
     'shared/helpers.lua'
-
 }
 
 
 client_scripts {
-
     'client/utilities.lua',
-
-    'client/camera.lua',
-
     'client/main.lua',
-
+    'client/camera.lua',
     'client/controls.lua'
-
 }
 
 
 server_scripts {
-
     'server/server.lua'
-
 }
 
 
 files {
-
     'html/index.html',
-
     'html/style.css',
-
     'html/app.js'
-
 }
 
 
 ui_page 'html/index.html'
-```
